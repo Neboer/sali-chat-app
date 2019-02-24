f70aeaf80a4b1272823a6d587ffa789051fc0786
Neboer <43609792+Neboer@users.noreply.github.com>
2019-02-24 17:54:48 +0800

    4.0版本客户端，配合3.0版本服务端 实现注册功能

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -8,26 +8,55 @@ import SaliChat from './SaliChat';
 
 // import logo from './logo.svg';
 class MainPage extends React.Component {
-    public state = {visible: false, loading: false, username: '', password: ''};
+    public state = {visible: false, loading: false, username: '', password: '', openState: 'login'};// openState:login/register
     public clickLoginButton = () => {
-        this.setState({visible: true})
+        this.setState({visible: true, openState: 'login'})
+    };
+    public clickRegisterButton = () => {
+        this.setState({visible: true, openState: 'register'})
     };
     public handleCancel = () => {
         this.setState({visible: false})
     };
     public handleLogin = () => { // 用户登录逻辑
-        this.setState({loading: true});
-        axios.post("http://"+url[environment]+":8080/login",{username:this.state.username},{headers:{'Accept': '*/*','Content-Type':'text/plain'}}).then(response=> {
-            switch(response.data){
-                case 'ok': {message.info("welcome,"+this.state.username);this.setState({loading:false});
-                    ReactDOM.render(<SaliChat username = {this.state.username}/>,document.getElementById('root') as HTMLElement);break;
+        axios.post("http://" + url[environment] + ":8080/login",
+            {username: this.state.username,password:this.state.password},
+            {headers: {'Accept': '*/*', 'Content-Type': 'text/plain'}}).then(response => {
+            switch (response.data) {
+                case 'ok': {
+                    message.success("welcome," + this.state.username);
+                    this.setState({loading: false});
+                    ReactDOM.render(<SaliChat
+                        username={this.state.username}/>, document.getElementById('root') as HTMLElement);
+                    break;
                 }
-                case 'no user': {message.error("invalid user name "+this.state.username);this.setState({loading:false});break;}
-                case 'wrong password': {message.error('wrong password');this.setState({loading:false});break;}
+                case 'no user': {
+                    message.error("invalid user name " + this.state.username);
+                    this.setState({loading: false});
+                    break;
+                }
+                case 'wrong password': {
+                    message.error('wrong password');
+                    this.setState({loading: false});
+                    break;
+                }
             }
-        }).catch(()=>{
+        }).catch(() => {
             message.error('connection error');
-            this.setState({loading:false})
+            this.setState({loading: false})
+        })
+    };
+    public handleRegister = () => { // 用户注册逻辑
+        axios.post("http://" + url[environment] + ":8080/register",
+            {username: this.state.username,password: this.state.password},
+            {headers: {'Accept': '*/*', 'Content-Type': 'text/plain'}}).then(response => {
+            if (response.data === 'ok') {
+                message.success('register granted');
+                this.handleLogin()
+            } else {
+                message.info('duplicated username');
+                this.setState({loading: false});
+            }
         })
     };
     public changeUsername = (event) => {
@@ -35,6 +64,14 @@ class MainPage extends React.Component {
     };
     public changePassword = (event) => {
         this.setState({password: event.target.value})
+    };
+    public handleSubmit = () => {
+        this.setState({loading: true});
+        if (this.state.openState === 'login') {
+            this.handleLogin()
+        } else {
+            this.handleRegister()
+        }
     };
 
     public render() {
@@ -45,13 +82,13 @@ class MainPage extends React.Component {
                     <h1 className="App-title">Welcome here!</h1>
                     <p><Button type={"primary"} icon={"login"} className={"buttons"} shape={"round"}
                                onClick={this.clickLoginButton}> Login </Button></p>
-                    <p><Button type={"default"} className={"buttons"} shape={"round"}><Icon type={"login"}
-                                                                                            rotate={270}/> Register</Button>
+                    <p><Button type={"default"} className={"buttons"} shape={"round"}
+                               onClick={this.clickRegisterButton}><Icon type={"login"} rotate={270}/> Register</Button>
                     </p>
-                    <Modal title={"Login"} visible={this.state.visible} onCancel={this.handleCancel}
+                    <Modal title={this.state.openState} visible={this.state.visible} onCancel={this.handleCancel}
                            footer={[<Button key={"back"} type={"default"} onClick={this.handleCancel}>cancel</Button>,
-                               <Button key={"submit"} type={"primary"} onClick={this.handleLogin}
-                                       loading={this.state.loading}>login</Button>]}>
+                               <Button key={'login'} type={"primary"} onClick={this.handleSubmit}
+                                       loading={this.state.loading}>{this.state.openState}</Button>]}>
                         input your username and password
                         <Input.Group size={"large"}>
                             <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
